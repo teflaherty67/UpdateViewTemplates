2be2047e6e31324071298544f41d580674dd6fff
Tom E Flaherty <teflaherty@utexas.edu>
2023-05-14 21:45:07 -0500

    Add project files.

---
+++ UpdateViewTemplates/Command.cs
@@ -0,0 +1,50 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+#endregion
+
+namespace UpdateViewTemplates
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            List<View> viewList = Utils.GetAllViews(doc);
+
+            List<View> vtList = Utils.GetAllViewTemplates(doc);
+
+            List<string> vtNames = Utils.GetAllViewTemplateNames(doc);
+
+            // if the project contains View Template names 00-Form/Foundation Plan
+
+            // then increase number by 1 for 07 - 15 & change 16 to 18
+
+            // update the Category parameter to match
+            
+            return Result.Succeeded;
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
---
+++ UpdateViewTemplates/Utils/Utils.cs
@@ -0,0 +1,86 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateViewTemplates
+{
+    internal static class Utils
+    {
+        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
+        {
+            RibbonPanel currentPanel = GetRibbonPanelByName(app, tabName, panelName);
+
+            if (currentPanel == null)
+                currentPanel = app.CreateRibbonPanel(tabName, panelName);
+
+            return currentPanel;
+        }        
+
+        internal static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
+        {
+            foreach (RibbonPanel tmpPanel in app.GetRibbonPanels(tabName))
+            {
+                if (tmpPanel.Name == panelName)
+                    return tmpPanel;
+            }
+
+            return null;
+        }
+
+        internal static List<View> GetAllViews(Document curDoc)
+        {
+            FilteredElementCollector m_colviews = new FilteredElementCollector(curDoc);
+            m_colviews.OfCategory(BuiltInCategory.OST_Views);
+
+            List<View> m_views = new List<View>();
+            foreach (View x in m_colviews.ToElements())
+            {
+                m_views.Add(x);
+            }
+
+            return m_views;
+        }
+
+        public static List<View> GetAllViewTemplates(Document curDoc)
+        {
+            List<View> returnList = new List<View>();
+            List<View> viewList = GetAllViews(curDoc);
+
+            //loop through views and check if is view template
+            foreach (View v in viewList)
+            {
+                if (v.IsTemplate == true)
+                {
+                    //add view template to list
+                    returnList.Add(v);
+                }
+            }
+
+            return returnList;
+        }
+
+        internal static List<string> GetAllViewTemplateNames(Document doc)
+        {
+            //returns list of view templates
+            List<string> viewTempList = new List<string>();
+            List<View> viewList = new List<View>();
+            viewList = GetAllViews(doc);
+
+            //loop through views and check if is view template
+            foreach (View v in viewList)
+            {
+                if (v.IsTemplate == true)
+                {
+                    //add view template to list
+                    viewTempList.Add(v.Name);
+                }
+            }
+
+            return viewTempList;
+        }
+    }
+}
